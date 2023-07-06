96e2c6f06049c562d58720c63aade9f88f22109f
marco paternosto frosi <marco@MacBook-Air-di-marco.local>
2023-07-06 08:54:00 +0200

    DayThree finished

--- DayThree/ex00/calculator.js
+++ DayThree/ex00/calculator.js
@@ -1,6 +1,6 @@
 function check(num)
 {
-	if (num < 0 || num == NaN)
+	if (num < 0 || isNaN(num))
 		return (1);
 	return 0;
 }
@@ -14,7 +14,7 @@ function doop()
 	let op = document.querySelector("#op").value;
 	let box = document.querySelector(".result");
 	if (check(left) || check(right))
-		alert("input must be positive")
+		alert("input must be positive integer no negative number or letter is allowed here!!!")
 	else
 	{
 		if (op == "+")
@@ -38,7 +38,7 @@ function doop()
 			{
 				document.querySelector("#video").style.visibility = "visible";
 				flag = 1;
-				document.querySelector("#video").src = "https://www.youtube.com/embed/T2U07KFqmew?&autoplay=1";
+				document.querySelector("#video").src = "https://www.youtube.com/embed/T2U07KFqmew?&autoplay=1&mute=1";
 			}
 			box.innerHTML = left / right;
 			console.log(left / right);
--- DayThree/ex01/shop.js
+++ DayThree/ex01/shop.js
@@ -1,76 +1,17 @@
-let data = [
+fetch("store.json")
+  .then(response => response.json())
+  .then(data =>
 	{
-		"image": "https://m.media-amazon.com/images/I/61j3SEUjMJL._AC_SX679_.jpg",
-		"title": "Echo Dot",
-		"description": "Il nostro Echo Dot con il migliore audio di sempre",
-		"category": "Audio",
-		"price": "64,99$"
-	},
-	{
-		"image": "https://m.media-amazon.com/images/I/91KA8TMeWRL.__AC_SX300_SY300_QL70_ML2_.jpg",
-		"title": "pokemon violetto",
-		"description": "Pokémon Scarlatto e Pokémon Violetto sono gli ultimissimi videogiochi della serie Pokémon",
-		"category": "Videogiochi",
-		"price": "49,50$"
-	}
-	,
-	{
-		"image": "https://m.media-amazon.com/images/I/61xhzvDIRKL.__AC_SX300_SY300_QL70_ML2_.jpg",
-		"title": "Nintendo Switch: Pro Controller",
-		"description": "Take your game sessions up a notch with the Nintendo Switch Pro Controller",
-		"category": "Videogiochi",
-		"price": "59,39$"
-	}
-	,
-	{
-		"image": "https://m.media-amazon.com/images/I/71cQYybJC7L._AC_SX679_.jpg",
-		"title": "HyperX Alloy Origins Core",
-		"description": "Switch meccanici HyperX con capacità nominale di 80 milioni di sequenze di tasti",
-		"category": "Pc/Computer",
-		"price": "69,99$"
-	}
-	,
-	{
-		"image": "https://m.media-amazon.com/images/I/61j3SEUjMJL._AC_SX679_.jpg",
-		"title": "Echo Dot",
-		"description": "Il nostro Echo Dot con il migliore audio di sempre",
-		"category": "Audio",
-		"price": "64,99$"
-	},
-	{
-		"image": "https://m.media-amazon.com/images/I/91KA8TMeWRL.__AC_SX300_SY300_QL70_ML2_.jpg",
-		"title": "pokemon violetto",
-		"description": "Pokémon Scarlatto e Pokémon Violetto sono gli ultimissimi videogiochi della serie Pokémon",
-		"category": "Videogiochi",
-		"price": "49,50$"
-	}
-	,
-	{
-		"image": "https://m.media-amazon.com/images/I/61xhzvDIRKL.__AC_SX300_SY300_QL70_ML2_.jpg",
-		"title": "Nintendo Switch: Pro Controller",
-		"description": "Take your game sessions up a notch with the Nintendo Switch Pro Controller",
-		"category": "Videogiochi",
-		"price": "59,39$"
-	}
-	,
-	{
-		"image": "https://m.media-amazon.com/images/I/71cQYybJC7L._AC_SX679_.jpg",
-		"title": "HyperX Alloy Origins Core",
-		"description": "Switch meccanici HyperX con capacità nominale di 80 milioni di sequenze di tasti",
-		"category": "Pc/Computer",
-		"price": "69,99$"
-	}
-]
-let clone = document.querySelector(".card_body");
-for (let i = 0; i < data.length; i++)
-{
-	let test = clone.cloneNode(clone);
-	test.querySelector(".image img").src = data[i].image
-	test.querySelector(".title h2").innerHTML = data[i].title
-	test.querySelector(".description h3").innerHTML = data[i].description
-	test.querySelector(".category h3").innerHTML = data[i].category
-	test.querySelector(".price h3").innerHTML = data[i].price
-	document.querySelector(".card_container").appendChild(test)
-}
-
-clone.remove();
+		let clone = document.querySelector(".card_body");
+		for (let i = 0; i < data.length; i++)
+		{
+			let test = clone.cloneNode(clone);
+			test.querySelector(".image img").src = data[i].image
+			test.querySelector(".title h2").innerHTML = data[i].title
+			test.querySelector(".description h3").innerHTML = data[i].description
+			test.querySelector(".category h3").innerHTML = data[i].category
+			test.querySelector(".price h3").innerHTML = data[i].price
+			document.querySelector(".card_container").appendChild(test)
+		}
+		clone.remove();
+	});
